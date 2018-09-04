589c2fbe7288c2866e4e0ea40156eefcba41b7ec
Gustavo <gustavohsborba@gmail.com>
2018-09-03 22:15:47 -0300

    adição de tts

---
+++ python_scripts/espeak_success.py
@@ -0,0 +1,20 @@
+'''
+Pré-requisitos:
+sudo apt-get update && sudo apt-get install espeak
+pip install pywin32 pypiwin32 pyttsx3
+'''
+
+import pyttsx3 # importamos o modulo pytts
+
+# meotodo init seleciona um ending de sintetização, no caso o espeak
+engine = pyttsx3.init()
+
+engine.setProperty('voice', 'brazil')  # mudamos a propriedade setando pelo id para pt-br
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate-40)
+engine.say('Seu desejo,') 
+engine.runAndWait()
+engine.say('e')
+engine.runAndWait()
+engine.say('uma, ordem!')
+engine.runAndWait()
