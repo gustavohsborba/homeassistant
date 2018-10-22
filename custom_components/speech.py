15bb581d78d262d398179e542432cca2993e472f
Gustavo <gustavohsborba@gmail.com>
2018-10-22 18:00:43 -0200

    adicionadas todas as automações que estão no texto, bem como seus componentes. Vamos torcer para dar certo

--- custom_components/speech.py
+++ custom_components/speech.py
@@ -4,37 +4,38 @@ Pré-requisitos:
 sudo apt-get update && sudo apt-get install espeak
 pip install pyttsx3
 '''
+
 import pyttsx3
 import logging
 import voluptuous as vol
 from homeassistant.helpers import config_validation as cv
 
+DOMAIN = "speech"
+SERVICE_SPEAK = "speak"
 REQUIREMENTS = ['pyttsx3']
 _LOGGER = logging.getLogger(__name__)
-
-DOMAIN = 'speech'
-SERVICE_SPEAK = 'speak'
 
 # ----------
 # Parameters
 # ----------
 
 CONF_VOICE = 'voice'
-CONF_RATE = 'speech_rate'
+CONF_SPEECH_RATE = 'speech_rate'
 
 # --------------
 # Default values
 # --------------
 
 DEFAULT_VOICE = 'brazil'
-DEFAULT_RATE = 200
-DEFAULT_MESSAGE = 'Não entendi'
+DEFAULT_SPEECH_RATE = 160
+DEFAULT_MESSAGE = ['Seu desejo,', 'e', 'uma, ordem!']
 
 # ----------------
 # Calls attributes
 # ----------------
 
 ATTR_MESSAGE = 'message'
+ATTR_SPEECH_RATE = 'speech_rate'
 
 # ------
 # Config
@@ -43,36 +44,39 @@ ATTR_MESSAGE = 'message'
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Optional(CONF_VOICE, DEFAULT_VOICE): cv.string,
-        vol.Optional(CONF_RATE, DEFAULT_RATE): int
+        vol.Optional(CONF_SPEECH_RATE, DEFAULT_SPEECH_RATE): int
     })
 }, extra=vol.ALLOW_EXTRA)
 
 
-# ------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
 # SETUP
 
 def setup(hass, config):
-    voice = config[DOMAIN].get(CONF_VOICE, DEFAULT_VOICE)
-    rate = config[DOMAIN].get(CONF_RATE, DEFAULT_RATE)
 
-    # ---------
-    # MAIN CALL
-    # ---------
+    voice = config[DOMAIN].get(CONF_VOICE, DEFAULT_VOICE)
+    speech_rate = config[DOMAIN].get(CONF_SPEECH_RATE, DEFAULT_SPEECH_RATE)
 
     def speak(call):
-        engine = pyttsx3.init()  # seleciona um engine de sintetização, default = pyttsx3
-        engine.setProperty('voice', voice)  # propriedade voice default: pt-br
-        engine.setProperty('rate', rate)  # Diminui a velocidade da fala, para ficar mais fluido
 
-        message = call.data.get(ATTR_MESSAGE, DEFAULT_MESSAGE)
-        engine.say(message)
-        engine.runAndWait()
-        _LOGGER.info('Speech Spoken Successfully: %s' % message)
+        instance_speech_rate = call.data.get(ATTR_SPEECH_RATE, speech_rate)
+        messages = call.data.get(ATTR_MESSAGE, DEFAULT_MESSAGE)
+        if not isinstance(messages, list):
+            messages = [messages]
+
+        engine = pyttsx3.init()
+        engine.setProperty('voice', voice)
+        engine.setProperty('rate', instance_speech_rate)
+
+        for message in messages:
+            engine.say(message)
+            engine.runAndWait()
+
+        _LOGGER.info('Spoken Successfully: %s' % ' '.join(messages).replace(',', ''))
         engine.stop()
 
-    # Registra o serviço no HomeAssistant
+    # Register our service with Home Assistant.
     hass.services.register(DOMAIN, SERVICE_SPEAK, speak)
-    _LOGGER.info('Started')
 
-    # Retorna um booleano indicando que a inicialização foi um sucesso
+    # Return boolean to indicate that initialization was successfully.
     return True
